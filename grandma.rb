d6a393a779d3f0f334e2d517d05d56a6c38fa7dc
Wills Blake <richwblake@gmail.com>
2019-11-04 17:12:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma( phrase )
   if phrase == "I LOVE YOU GRANDMA"
-    "NO, NOT SINCE 1938"
-  elsif phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938"
   else
     "HUH?! SPEAK UP, SONNY"
   end
